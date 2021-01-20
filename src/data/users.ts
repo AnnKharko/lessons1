348028eeb8426d2bc0e31001ef73612fed245eaf
Ann <kharko2112.mechmat@gmail.com>
2021-01-21 00:18:03 +0200

    lessons1

---
+++ src/data/users.ts
@@ -0,0 +1,13 @@
+export const users = [
+  {name: 'Anastasia', age: 18, footSize : 35},
+  {name: 'Beatrice', age: 17, footSize : 36},
+  {name: 'Diana', age: 19 , footSize : 35},
+  {name: 'Elizabeth', age: 15, footSize : 34},
+  {name: 'Elsa', age: 20, footSize : 37},
+  {name: 'Anna', age: 16, footSize: 35},
+  {name: 'Cinderella', age: 16, footSize: 33},
+  {name: 'Fiona', age: 17, footSize: 38},
+  {name: 'Isabella', age: 15, footSize: 34},
+  {name: 'Jasmine', age: 19, footSize: 36}
+];
+
